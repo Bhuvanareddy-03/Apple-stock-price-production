43a6a90ab2850f26b015b6650146c19c6c659bb5
Bhuvanareddy-03 <surupureddybhubaneswari@gmail.com>
2025-10-24 14:36:37 +0530

    Delete 1_Model_Comparison.py

--- 1_Model_Comparison.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-from utils import load_data, evaluate_models
-
-st.title("📊 Model Performance Comparison")
-
-df = load_data()
-comparison_df = evaluate_models(df)
-st.dataframe(comparison_df)
